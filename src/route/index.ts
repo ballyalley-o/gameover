d18e5f9a7812acc42544346a62318c670e0f5daf
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-12-31 01:08:20 +1000

    [route]: mounted game route

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,9 +1,10 @@
 import { Application } from 'express'
 import { linkAuthRoute } from 'route/auth'
+import { linkGameRoute } from 'route/game'
 
 export const mainRoute = (app: Application, apiVer: string) => {
     linkAuthRoute(app, apiVer)
+    linkGameRoute(app, apiVer)
 }
 
 export { default as ServerStatic } from './server-static'
-
