4978b7863e2d9b0de5972e87c438bbe9dc6e676d
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-12-31 01:01:08 +1000

    [route]: added route for lineup

---
+++ src/route/game/lineup.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import { LineupController } from 'controller'
+
+const router = Router({ mergeParams: true })
+
+router.route('/')
+  .post(LineupController.create)
+
+router.route('/:id')
+  .get(LineupController.get)
+  .put(LineupController.update)
+
+router.route('/:id/metrics')
+  .post(LineupController.computeMetrics)
+
+export default router
