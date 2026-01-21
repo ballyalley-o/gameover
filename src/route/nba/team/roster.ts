8e48c3ddd5982d62c54dca2ec19c65147ee81890
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-01-21 20:35:25 +1000

    [enhance-refactor]: update authorization for roster management routes to restrict access to admin users

--- src/route/nba/team/roster.ts
+++ src/route/nba/team/roster.ts
@@ -1,17 +1,18 @@
 import { Router } from 'express'
-import { protect } from 'middleware'
+import { protect, authorize } from 'middleware'
 import { TeamController } from 'controller'
 
 const router = Router({ mergeParams: true })
 
 router.route('/')
   .get(protect, TeamController.roster)
-  .post(protect, TeamController.addPlayer)
+  .post(protect, authorize('admin'), TeamController.addPlayer)
+  .delete(protect, authorize('admin'), TeamController.deleteRosterAll)
 
-router.post('/bulk', protect, TeamController.addPlayerToRosterBulk)
+router.put('/sync', protect, authorize('admin'), TeamController.syncRoster)
+router.post('/bulk', protect, authorize('admin'), TeamController.addPlayerToRosterBulk)
 
-router.route('/:playerId')
-  .delete(protect, TeamController.removePlayer)
+router.route('/:playerId').delete(protect, authorize('admin'), TeamController.removePlayer)
 
 /**
  * @path {apiUrl}/nba/team/:key/roster
