b641b1a4a326a5cb1cc9bc67fbcb93d4a9ec9231
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-01-24 00:49:51 +1000

    [fix-consistency]

--- src/route/auth/index.ts
+++ src/route/auth/index.ts
@@ -1,12 +1,11 @@
 import { Application } from 'express'
+import { MODULE, PATH_PARAM } from 'config/dir'
 import userRoute from 'route/auth/user'
 import authRoute from 'route/auth/auth'
-import { MODULE } from 'config/dir'
 import { combine } from 'utility'
-
 
 export const linkAuthRoute = (app: Application, apiVer: string) => {
     const base = combine(apiVer, MODULE.AUTH)
     app.use(base, authRoute)
-    app.use(combine(base, MODULE.USER), userRoute)
+    app.use(combine(base, PATH_PARAM.USER), userRoute)
 }
