b984be47d6c7d84b8d5abfaf7fd0fb820eac3bc6
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-01-23 13:23:30 +1000

    [route]: added archetype sync and player stats refresh routes

--- src/route/nba/player.ts
+++ src/route/nba/player.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { PlayerController } from 'controller'
 import { authorize, protect } from 'middleware'
+import { combine } from 'utility'
 
 const router = Router({ mergeParams: true })
 
@@ -11,7 +12,10 @@ router.route('/')
 router.post('/create-basic', protect, PlayerController.createBasic)
 
 router.put('/archetype/refresh', protect, authorize('admin'), PlayerController.refreshArchetype)
+router.put(combine('/', 'archetype', 'refresh', ':playerId'), protect, authorize('admin'), PlayerController.refreshArchetypeByPlayerId)
+
 router.put('/stats/refresh', protect, authorize('admin'), PlayerController.syncAllStats)
+router.put(combine('/', 'stats', 'refresh', ':playerId'), protect, authorize('admin'), PlayerController.syncPlayerStatsByPlayerId)
 
 router.route('/:playerId')
   .get(PlayerController.get)
