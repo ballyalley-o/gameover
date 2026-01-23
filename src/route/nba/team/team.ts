866446583b5e0dc4135fb61fcfcf9e0301d74713
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-01-24 00:54:34 +1000

    [refactor-route] update route paths to use PATH_PARAM and combinePathParam

--- src/route/nba/team/team.ts
+++ src/route/nba/team/team.ts
@@ -1,18 +1,19 @@
 import { Router } from 'express'
 import { TeamController } from 'controller'
 import { protect, authorize } from 'middleware'
+import { combinePathParam, PATH_PARAM } from 'config/dir'
 
 const router = Router({ mergeParams: true })
 
-router.route('/')
+router.route(PATH_PARAM.ROOT)
   .get(protect, TeamController.list)
   .post(protect, authorize('admin'), TeamController.create)
   .delete(protect, authorize('admin'), TeamController.deleteTeamAll)
 
 router
-  .post('/create-basic', protect, authorize('admin'), TeamController.createBasic)
+  .post(combinePathParam('create-basic'), protect, authorize('admin'), TeamController.createBasic)
 
-router.route('/:teamId')
+router.route(combinePathParam(':teamId'))
   .get(protect, TeamController.get)
   .put(protect, authorize('admin'),TeamController.update)
 
