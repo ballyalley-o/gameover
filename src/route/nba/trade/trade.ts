fa4ef0ff791544f2d2cbfdfe330191d5d8d40e4d
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-01-26 16:10:35 +1000

    [enhance]: history routes added

--- src/route/nba/trade/trade.ts
+++ src/route/nba/trade/trade.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 import { TradeController } from 'controller'
 import { combinePathParam } from 'config/dir'
+import { authorize, protect } from 'middleware'
 
 const router = Router({ mergeParams: true })
 
 router.post(combinePathParam('preview'), TradeController.preview)
 router.post(combinePathParam('execute'), TradeController.execute)
 router.post(combinePathParam('suggest'), TradeController.suggest)
+router.get(combinePathParam('history'), TradeController.history)
+router.delete(combinePathParam('history'), protect, authorize('admin'), TradeController.resetHistory)
 
 export default router
