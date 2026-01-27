6b85cc4e79dec069c9c8ac64773eedfabe5206ac
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-01-27 23:46:07 +1000

    [enhance]

--- src/types/schema.d.ts
+++ src/types/schema.d.ts
@@ -1,4 +1,4 @@
-import { users, players } from "db/schema"
+import { users, players, teams, myLeagues, myLeagueTeams, myLeaguePlayers, myLeagueRosters } from "db/schema"
 
 export type RosterItemInput = {
   playerId    : string
@@ -7,9 +7,17 @@ export type RosterItemInput = {
   isActive   ?: boolean
 }
 
-export type DrizzleUser      = typeof users.$inferSelect
-export type NewDrizzleUser   = typeof users.$inferInsert
-export type DrizzlePlayer    = typeof players.$inferSelect
-export type NewDrizzlePlayer = typeof players.$inferInsert
-export type DrizzleTeam      = typeof teams.$inferSelect
-export type NewDrizzleTeam   = typeof teams.$inferInsert
+export type DrizzleUser              = typeof users.$inferSelect
+export type NewDrizzleUser           = typeof users.$inferInsert
+export type DrizzlePlayer            = typeof players.$inferSelect
+export type NewDrizzlePlayer         = typeof players.$inferInsert
+export type DrizzleTeam              = typeof teams.$inferSelect
+export type NewDrizzleTeam           = typeof teams.$inferInsert
+export type DrizzleMyLeague          = typeof myLeagues.$inferSelect
+export type NewDrizzleMyLeague       = typeof myLeagues.$inferInsert
+export type DrizzleMyLeagueTeam      = typeof myLeagueTeams.$inferSelect
+export type NewDrizzleMyLeagueTeam   = typeof myLeagueTeams.$inferInsert
+export type DrizzleMyLeaguePlayer    = typeof myLeaguePlayers.$inferSelect
+export type NewDrizzleMyLeaguePlayer = typeof myLeaguePlayers.$inferInsert
+export type DrizzleMyLeagueRoster    = typeof myLeagueRosters.$inferSelect
+export type NewDrizzleMyLeagueRoster = typeof myLeagueRosters.$inferInsert
