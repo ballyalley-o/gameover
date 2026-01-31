d6c25b8f57bd9b64a5bb5dc74938f4e69c00e8bf
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-01-31 16:58:52 +1000

    [routing]

--- src/route/myleague/myleague.ts
+++ src/route/myleague/myleague.ts
@@ -1,11 +1,23 @@
 import { Router } from 'express'
 import { MyLeagueController } from 'controller'
 import { combinePathParam, PATH_PARAM } from 'config/dir'
+import { authorize, protect } from 'middleware'
 
 const router = Router({ mergeParams: true })
 
-router.post(PATH_PARAM.ROOT, MyLeagueController.create)
-router.post(combinePathParam(PATH_PARAM.LEAGUE_ID, PATH_PARAM.DRAFT), MyLeagueController.draft)
+router
+    .route(PATH_PARAM.ROOT)
+    .get(protect, MyLeagueController.list)
+    .post(protect, MyLeagueController.create)
+    .delete(protect, authorize('admin'), MyLeagueController.deleteAll)
+
+router
+    .route(combinePathParam(PATH_PARAM.MY_LEAGUE_ID))
+    .get(protect, authorize('admin'), MyLeagueController.get)
+    .put(protect, authorize('admin'), MyLeagueController.update)
+    .delete(protect, authorize('admin'), MyLeagueController.deleteMyLeagueById)
+
+router.post(combinePathParam(PATH_PARAM.MY_LEAGUE_ID, PATH_PARAM.DRAFT), MyLeagueController.draft)
 
 
 /**
