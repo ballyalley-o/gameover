e0fd12c1cbf4b7f548f35057dfcff1f98130e772
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-02-05 10:26:30 +1000

    [structure]: refactor file structure

---
+++ src/controller/auth/auth.controller.ts
@@ -0,0 +1,100 @@
+import { Request, Response, NextFunction } from 'express'
+import { eq } from 'drizzle-orm'
+import { GLOBAL, db } from 'gameover'
+import bcrypt from 'bcryptjs'
+import { Service } from 'controller'
+import { ErrorResponse } from 'middleware'
+import { CODE, fiveSecFromNow, REGEX, Resp, RESPONSE } from 'constant'
+import { transl } from 'utility'
+import { users } from '../../db/schema'
+
+const TAG = 'Auth.Controller'
+export class AuthController {
+    public static async signIn(req: Request, res: Response, _next: NextFunction) {
+        try {
+            const { email, password }  = req.body
+
+            if (!email || !password) Service.invalid()
+            const [user]  = await db.select().from(users).where(eq(users.email, email))
+
+            if (!user) Service.invalid()
+            const isMatch = await bcrypt.compare(password, user.password || '')
+
+            if (!isMatch) Service.invalid()
+
+            const userFiltered = { id: user.id, name: `${user.firstname} ${user.lastname}`, email: user.email, role: user.role }
+            await Service.sendTokenResponse(userFiltered, CODE.OK, res)
+        } catch (error: any) {
+            Service.catchError(error, TAG, 'signIn', res)
+        }
+    }
+
+    public static async signUp(req: Request, res: Response, _next: NextFunction) {
+        try {
+            const { email }    = req.body
+            const [userExists] = await db.select({ id: users.id }).from(users).where(eq(users.email, email))
+
+            if (userExists) Service.alreadyExist(email)
+            if (!req.body.password) {
+                throw new ErrorResponse(RESPONSE.ERROR.INVALID_CREDENTIALS, CODE.BAD_REQUEST)
+            }
+            /**
+             * Password Requirements:
+                - Minimum of 8 characters
+                - At least one uppercase letter (A–Z)
+                - At least one lowercase letter (a–z)
+                - At least one number (0–9)
+                - At least one special character (e.g., !, @, #, $)
+                */
+            const dataPassword   = String(req.body.password)
+            const PASSWORD_REGEX = new RegExp(REGEX.PASSWORD)
+
+            if (!PASSWORD_REGEX.test(dataPassword)) {
+                throw new Error(transl('validation.password'))
+            }
+
+            const hashedPassword  = await bcrypt.hash(req.body.password, GLOBAL.HASH.SALT_ROUNDS)
+            const [newUser]       = await db.insert(users).values({ ...req.body, password: hashedPassword }).returning()
+            const newUserFiltered = { name: `${newUser.firstname} ${newUser.lastname}`, email: newUser.email, role: newUser.role }
+            await Service.sendTokenResponse(newUserFiltered, CODE.CREATED, res)
+        } catch (error: any) {
+            Service.catchError(error, TAG, 'signUp', res)
+        }
+    }
+
+    public static async signOut(_req: Request, res: Response, _next: NextFunction) {
+        res.cookie(GLOBAL.COOKIE.NAME, "none", {
+            expires : fiveSecFromNow,
+            httpOnly: true
+        })
+
+        try {
+            res.status(CODE.OK).send(Resp.TokenResponse('', {}))
+        } catch (error: any) {
+            Service.catchError(error, TAG, 'signOut', res)
+        }
+    }
+
+    public static async myAccount(req: Request, res: Response, _next: NextFunction) {
+        try {
+            if (!req.user?.id) {
+                return Service.invalid(RESPONSE.ERROR[401], CODE.UNAUTHORIZED)
+            }
+            const [user] = await db.select().from(users).where(eq(users.id, req.user.id))
+            if (!user) Service.notFound()
+            res.status(CODE.OK).send(Resp.Ok(user))
+        } catch (error: any) {
+            Service.catchError(error, TAG, 'myAccount', res)
+        }
+    }
+
+    public static async updateAccount(req: Request, res: Response, _next: NextFunction) {
+        try {
+            const [updatedUser] = await db.update(users).set(req.body).where(eq(users.id, req.user.id)).returning()
+            if (!updatedUser) Service.notFound()
+            res.status(CODE.OK).send(Resp.Ok(updatedUser))
+        } catch (error: any) {
+            Service.catchError(error, TAG, 'updateAccount', res)
+        }
+    }
+}
