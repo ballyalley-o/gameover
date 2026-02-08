e9b63b7ba461db29f39b30a19a96c966d5e39c78
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-02-08 11:09:31 +1000

    [cleanup]

--- src/route/nba/index.ts
+++ src/route/nba/index.ts
@@ -1,10 +1,9 @@
-import { Application, Router } from 'express'
+import { Application } from 'express'
 import { MODULE, PATH_PARAM } from 'config/dir'
 import { combine } from 'utility'
 
 import teamRoute from './team'
 import playerRoute from './player'
-import lineupRoute from './lineup'
 import gameRoute from './game'
 import tradeRoute from './trade'
 
@@ -12,7 +11,6 @@ export const linkNBARoute = (app: Application, apiVer: string) => {
   const base = combine(apiVer, MODULE.NBA)
   app.use(combine(base, PATH_PARAM.PLAYER), playerRoute)
   app.use(combine(base, PATH_PARAM.TEAM), teamRoute)
-  app.use(combine(base, PATH_PARAM.LINEUP), lineupRoute)
   app.use(combine(base, PATH_PARAM.GAME), gameRoute)
   app.use(combine(base, PATH_PARAM.TRADE), tradeRoute)
 }
